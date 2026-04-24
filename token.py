437764e50b57420bdd8767f13415bb4dc19c9044
zax687767 <zax687767@gmail.com>
2026-04-24 18:31:09 +0200

    Update token.py

--- token.py
+++ token.py
@@ -1,7 +1,7 @@
 # demo_secret_scanning.py
 # UWAGA: To jest FAŁSZYWY token używany WYŁĄCZNIE do demo secret scanningu
 
-GITHUB_TOKEN = "github_pat_11AA22BB33CC44DD55EE66FF77GG88HH99II00JJ"
+GITHUB_TOKEN = "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"
 
 def main():
     print("Hello from secret scanning demo!")
